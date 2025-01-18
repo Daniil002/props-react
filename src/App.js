6f1bd9c4b2e10080d0d78742231d5e08bd093215
Daniil <prokopenkodaniil789@gmail.com>
2025-01-18 21:52:07 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import PetInfo from './components/Petinfo';
+
+
+function App() {
+  /* родительский компонент передает в дочерний свойсво (props) */
+  return (
+    <div className="App">
+      <PetInfo animal='cat' age='11' />
+    </div>
+  );
+}
+
+export default App;
